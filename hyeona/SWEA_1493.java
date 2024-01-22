268d7f51c10a4e18f9371bd24c2efdb08eb457be
chahyeonnaa <gusdk21650@naver.com>
2024-01-22 23:59:03 +0900

    1회차-2,23,28

---
+++ hyeona/SWEA_1493.java
@@ -0,0 +1,59 @@
+package algorithm.Algorithm_Study.hyeona;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SWEA_1493 {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        System.setIn(new FileInputStream("/Users/hyeonacha/Desktop/ssafy_work/src/algorithm/Algorithm_Study/hyeona/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i=0; i<T; i++)
+        {
+            int P = sc.nextInt();
+            int result[] = cal_loaction(P);
+
+            int x = result[0];
+            int y = result[1];
+
+
+            int Q = sc.nextInt();
+            int result2[] = cal_loaction(Q);
+
+            int z = result2[0];
+            int w = result2[1];
+
+            int nx = x+z;
+            int ny = y+w;
+
+            int answer =0;
+            for(int k=1; k< nx+ny; k++)
+            {
+                answer += k;
+            }
+            System.out.printf("#%d %d", i+1, answer-=(ny-1));
+            System.out.println();
+
+        }
+    }
+
+    public static int[] cal_loaction(int X)
+    {
+        int number = 0;
+        int i=1;
+        while(number<X)
+        {
+            number += i;
+            i+=1;
+        }
+
+        int index = number-X;
+
+        int[] location = {i-1-(index), 1+index};
+        return location;
+
+    }
+
+}
---
+++ hyeona/SWEA_7964.java
@@ -0,0 +1,42 @@
+package algorithm.Algorithm_Study.hyeona;
+
+import java.util.Scanner;
+
+public class SWEA_7964 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i=0; i<T; i++)
+        {
+            int N = sc.nextInt();
+            int D = sc.nextInt();
+            int answer =0;
+            int[] arr = new int[N];
+            for(int j=0; j<N; j++)
+            {
+                arr[j] = sc.nextInt();
+            }
+
+            int index=0;
+            // D 칸을 왔으면 1로 만들어줘야하고, 1을 만나면 칸 수를 초기화한다
+
+            for(int k=0; k<N; k++)
+            {
+                index +=1;
+                if(index>=D && arr[k]==0)
+                {
+                    answer +=1;
+                    index=0;
+                }
+                if(arr[k]==1)
+                {
+                    index=0;
+                }
+            }
+            System.out.printf("#%d %d", i+1, answer);
+            System.out.println();
+
+        }
+
+    }
+}
