e24f2a82bf5fb8d14212457d8d70be4b0a71b1ac
minkyung <minkyung7365@gmail.com>
2024-01-23 22:19:34 +0900

    update directory

--- minkyung/src/_240123/swea_1493.java
+++ minkyung/src/_240123/swea_1493.java
@@ -1,9 +1,8 @@
-package minkyung.swea;
+package _240123;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class swea_1493 {
