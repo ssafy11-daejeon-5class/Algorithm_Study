b882f9fd7b4489ed39ab9bab65d2d8b7d9aa8d55
chahyeonnaa <gusdk21650@naver.com>
2024-01-25 00:05:41 +0900

    feat: BOJ_10158 / SWEA_1860

---
+++ hyeona/_240125/BOJ_10158.java
@@ -0,0 +1,32 @@
+package algorithm.Algorithm_Study.hyeona._240125;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class BOJ_10158 {
+    private static final BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(bufferReader.readLine());
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(bufferReader.readLine());
+        int P = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        int T = Integer.parseInt(bufferReader.readLine());
+        P = (P+T)%(2*W);
+        Q = (Q+T)%(2*H);
+
+        if(P>W) P=Math.abs(W*2-P);
+        if(Q>H) Q=Math.abs(H*2-Q);
+
+
+        System.out.print(P+" "+Q);
+
+
+    }
+}
