4d9b5abc559b525467b3386a0cccae3b245ec3e3
kwonja <kownja87@gmail.com>
2024-02-04 23:16:56 +0900

    feat : BOJ_16234_인구이동 시간초과

--- src/study/_240205/sungmin/BOJ16234.java
+++ src/study/_240205/sungmin/BOJ16234.java
@@ -3,6 +3,8 @@ package study._240205.sungmin;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -10,7 +12,14 @@ public class BOJ16234 {
 	static int n;
 	static Queue<Pair> q;
 	static int[][] board;
-	static int[][] dist;
+	static int[][] visited;
+	static int l;
+	static int r;
+	static int sum;
+	static int place;
+	static int move=-1;
+	static int check=1;
+	static int seq;
 	static int[] dx=new int[] {1,-1,0,0};
 	static int[] dy=new int[] {0,0,1,-1};
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -20,11 +29,10 @@ public class BOJ16234 {
 		StringTokenizer st= new StringTokenizer(br.readLine());
 		
 		n= Integer.parseInt(st.nextToken());
-		int l= Integer.parseInt(st.nextToken());
-		int r= Integer.parseInt(st.nextToken());
+		l= Integer.parseInt(st.nextToken());
+		r= Integer.parseInt(st.nextToken());
 		
 		board=new int[n][n];
-		dist=new int[n][n];
 		for(int i=0;i<n;i++)
 		{
 			st= new StringTokenizer(br.readLine());
@@ -34,8 +42,76 @@ public class BOJ16234 {
 			}
 		}
 		
-		//입력받기
 		
+		while(check==1)
+		{
+			visited=new int[n][n];
+			move++;
+			check=0;
+			for(int[] s : board)
+			{
+				System.out.println(Arrays.toString(s));				
+			}
+			for(int i=0;i<n;i++)
+			{
+				for(int j=0;j<n;j++)
+				{
+					if(visited[i][j]==0)
+					{
+						place=0;
+						sum=0; 
+						q=new LinkedList<Pair>();
+						bfs(i,j,++seq);
+						for(int k=0;k<n;k++)
+						{
+							for(int g=0;g<n;g++)
+							{
+								if(visited[k][g]==seq) //방문한 왕국
+								{
+									board[k][g]=sum/place; //인구수 나누기
+								}
+							}
+						}
+						
+					}
+				}
+			}
+			if(check==0) break; //인구이동x
+		}
+		System.out.print(move);
+		
+		
+	}
+	public static void bfs(int x,int y,int time)
+	{
+		q.offer(new Pair(x,y));
+		visited[x][y]=1;
+		sum += board[x][y];
+		place=1;
+		while(!q.isEmpty())
+		{
+			int curx = q.peek().getX();
+			int cury = q.peek().getY();
+			q.poll();
+			for(int i=0;i<4;i++)
+			{
+				int nx = curx + dx[i];
+				int ny = cury + dy[i];
+			
+				if(nx<0 || nx>=n || ny<0 || ny>=n)continue;
+				if(visited[nx][ny]>0)continue;
+				//L명이상 R명이하일때 관문을 연다
+				if(Math.abs(board[curx][cury]-board[nx][ny])<=r && Math.abs(board[curx][cury]-board[nx][ny])>=l)
+				{
+					
+					q.offer(new Pair(nx,ny));
+					visited[nx][ny]=time;
+					place++;
+					sum += board[nx][ny];
+					check=1; //인구이동 발생
+				}
+			}
+		}
 	}
 
 }
