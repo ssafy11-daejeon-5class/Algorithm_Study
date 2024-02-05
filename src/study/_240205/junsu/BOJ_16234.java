debbbb6acc07c37f957b24690a19c3a3644529f4
dlwnstn0524 <ljs052450@gmail.com>
2024-02-05 19:32:23 +0900

    [FEAT] 인구 이동 풀이 완료

--- src/study/_240205/junsu/BOJ_16234.java
+++ src/study/_240205/junsu/BOJ_16234.java
@@ -10,11 +10,21 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_16234 {
+	
+	static class Node {
+		int i;
+		int j;
+		public Node(int i, int j){
+			this.i = i;
+			this.j = j;
+		}
+	}
 
-	static int N, L, R, cnt;
+	static int N, L, R;
 	static int[][] maps;
 	static StringTokenizer st;
 	static boolean[][] visited;
+	static Queue<Node> list;
 	static int[] di = { -1, 1, 0, 0 };
 	static int[] dj = { 0, 0, -1, 1 };
 
@@ -33,65 +43,66 @@ public class BOJ_16234 {
 			}
 		}
 
-		migration();
+		System.out.println(move());
 
 	}
 
-	private static void migration() {
-		// TODO Auto-generated method stub
-		cnt = 0;
-		visited = new boolean[N][N];
-		while (movePeople()) {
-			cnt++;
-			visited = new boolean[N][N];
+	private static int move() {
+		int result = 0;
+        while(true) {
+            boolean isMove = false;
+            visited = new boolean[N][N];
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < N; j++) {
+                    if(!visited[i][j]) {
+                        int sum = bfs(i, j); //bfs탐색으로 열릴 수 있는 국경선 확인 하며 인구 이동할 총 인구수 반환
+                        if(list.size() > 1) {
+                            changePopulation(sum); //열린 국경선 내의 노드들 인구 변경
+                            isMove = true;
+                        }    
+                    }
+                }
+            }
+            if(!isMove) return result;
+            result++;
+        }
+	}
+
+	private static void changePopulation(int sum) {
+		int avg = sum / list.size();
+		while(!list.isEmpty()){
+			Node temp = list.poll();
+			maps[temp.i][temp.j] = avg;
 		}
-		System.out.println(cnt);
 	}
 
-	private static boolean movePeople() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (!visited[i][j]) {
-					bfs(i, j);
-				}
-			}
-		}
-		return false;
-	}
+	private static int bfs(int i, int j) {
+		Queue<Node> q = new ArrayDeque<>();
+		list = new ArrayDeque<>();
 
-	private static void bfs(int i, int j) {
-		// TODO Auto-generated method stub
-		Queue<int[]> q = new ArrayDeque<>();
-		Queue<int[]> visit = new ArrayDeque<>();
-		int[] arr = new int[2];
-		arr[0] = i;
-		arr[1] = j;
+		q.offer(new Node(i,j));
+		list.add(new Node(i, j));
+		visited[i][j] = true;
 
-		int count = 1;
 		int sum = maps[i][j];
+		while(!q.isEmpty()){
+			Node current = q.poll();
 
-		q.offer(arr);
-		while (!q.isEmpty()) {
-			int[] info = q.poll();
-			for (int k = 0; k < 4; k++) {
-				int ni = info[0] + di[k];
-				int nj = info[1] + di[k];
-				if (ni >= 0 && ni < N && nj >= 0 && nj < N && !visited[ni][nj]) {
-					int gap = Math.abs(maps[ni][nj] - maps[info[0]][info[1]]);
-					if (gap >= L && gap <= R) {
-						q.offer(new int[] { ni, nj });
-						visit.offer(new int[] { ni, nj });
-						sum += maps[ni][nj];
-						count++;
-					}
+			for(int k = 0 ; k < 4 ; k++){
+				int ni = current.i + di[k];
+				int nj = current.j + dj[k];
+
+				if(ni >= 0 && ni < N && nj >= 0 && nj < N && !visited[ni][nj]){
+					int diff = Math.abs(maps[current.i][current.j] - maps[ni][nj]);
+                    if(L <= diff && diff <= R) {
+                        q.offer(new Node(ni, nj));
+                        list.offer(new Node(ni, nj));
+                        sum += maps[ni][nj];
+                        visited[ni][nj] = true;
+                    }     
 				}
 			}
 		}
-		
-		while(!visit.isEmpty()) {
-			int[] temp = visit.poll();
-			maps[temp[0]][temp[1]] = sum / count;
-		}
-
+		return sum;
 	}
 }
