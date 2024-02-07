84f62462e2b9cf08effbd39b7f9556056a1fe4f0
kwonja <kownja87@gmail.com>
2024-02-07 23:17:29 +0900

    feat : 색종이,정사각형 문제풀이

--- src/hw/_240207/kwonja/SWEA_1861.java
+++ src/hw/_240207/kwonja/SWEA_1861.java
@@ -3,6 +3,8 @@ package hw._240207.kwonja;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class SWEA_1861 {
@@ -16,6 +18,7 @@ public class SWEA_1861 {
 	static int[][] visitied;
 	static int n;
 	static int res=Integer.MIN_VALUE;
+	static Queue<Pair> q= new ArrayDeque<Pair>();
 	static int x,y;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,6 +31,8 @@ public class SWEA_1861 {
 			n =Integer.parseInt(br.readLine());
 			board = new int[n][n];
 			visitied = new int[n][n];
+			res=Integer.MIN_VALUE;
+			x=y=-1;
 			for(int i=0;i<n;i++)
 			{
 				st=new StringTokenizer(br.readLine());
@@ -41,34 +46,55 @@ public class SWEA_1861 {
 			{
 				for(int j=0;j<n;j++)
 				{
-					visitied[i][j]=1;
-					dfs(i,j,1,i,j);
-					visitied[i][j]=0;
+					bfs(i,j);
+					visitied = new int[n][n];
 				}
 			}
 			System.out.println("#" +test_case +" "+board[x][y] +" "+res);
 		}
 		
 	}
-	private static void dfs(int curx, int cury,int count,int posx,int posy) {
+	private static void bfs(int startx ,int starty) {
 		
-		if(count>res)
+		
+		q.offer(new Pair(startx,starty));
+		visitied[startx][starty]=1;
+		while(!q.isEmpty())
 		{
-			res=count;
-			x=posx;
-			y=posy;
-		}
-		for(int i=0;i<4;i++)
-		{
-			int nx = curx + dx[i];
-			int ny = cury + dy[i];
-			if(nx<0 || nx>=n || ny<0 || ny>=n)continue;
-			if(visitied[nx][ny]==1)continue;
-			if((board[nx][ny]-board[curx][cury])==1)
+			int curx=q.peek().getX();
+			int cury=q.peek().getY();
+			q.poll();
+			for(int i=0;i<4;i++)
 			{
-				visitied[nx][ny]=1;
-				dfs(nx,ny,count+1,posx,posy);
-				visitied[nx][ny]=0;
+				int nx = curx + dx[i];
+				int ny = cury + dy[i];
+				if(nx<0 || nx>=n || ny<0 || ny>=n)continue;
+				if(visitied[nx][ny]>0)continue;
+				if((board[nx][ny]-board[curx][cury])==1)
+				{
+					q.offer(new Pair(nx,ny));
+					visitied[nx][ny] = visitied[curx][cury]+1;
+					if(visitied[nx][ny]==res)
+					{
+//						System.out.println(board[startx][starty] +" "+ res);
+						res=visitied[nx][ny];
+						if(x==-1)
+						{
+							x=startx;
+							y=starty;
+						}
+						else if(board[startx][starty]<=board[x][y])
+						{
+							x=startx;
+							y=starty;	
+						}
+					}else if(visitied[nx][ny] > res)
+					{
+						res=visitied[nx][ny];
+						x=startx;
+						y=starty;
+					}
+				}
 			}
 		}
 	}
--- src/study/_240205/sungmin/SWEA_5563.java
+++ src/study/_240205/sungmin/SWEA_5563.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 /*
  * SWEA 줄기 세포 배양
+ * 우선순위 큐를 배우고 다시해보자!
  */
 public class SWEA_5563 {
 	
@@ -27,7 +28,59 @@ public class SWEA_5563 {
 			int m=Integer.parseInt(st.nextToken());
 			int k=Integer.parseInt(st.nextToken());
 			
+			for(int i=0;i<n;i++)
+			{
+				st= new StringTokenizer(br.readLine());
+				for(int j=0;j<n;j++)
+				{
+					board[150 + i][150 +j]=Integer.parseInt(st.nextToken());
+					if(board[150 + i][150 +j] != 0)
+					{
+						q.offer(new Ceil(150+i,150+j,timer));
+						board[150 + i][150 +j]=1; //방문 처리
+					}
+				}
+			}
+			//배양 시작
+			bfs();
 		}
+	}
+	public static void bfs()
+	{
+		int qsize =q.size();
+		timer++;
+		
+		while(true)
+		{
+			//한번씩 이동할수 있는지 확인한다.
+			for(int i=0;i<q.size();i++)
+			{
+				int curx =q.peek().getX();
+				int cury =q.peek().getY();
+				
+				
+				for(int j=0;j<4;j++)
+				{
+					int nx= curx+dx[j];
+					int ny = cury+dy[j];
+					if(nx<0 || nx>=350 || ny<0 || ny>=350)continue;
+					if(visitied[nx][ny]==1 || board[ny][ny]!=0) continue; //배양을 했다면 패스
+					
+					
+					//활성일때
+					
+					//비활성 일때
+					
+					//죽었을때
+					q.offer(new Ceil(ny,ny,timer));//배양
+					board[ny][ny]=board[curx][cury];
+				}
+			}
+		}
+		//q에 들어 있는 대기열이 한번씩 다 돌면 1시간치 배양이 진행된다.
+		//배양이 시작할때 timer를 올리고, 증식할수 있는지 판단
+		//판단 방법
+		//현재 타이머-증식해서 비활성화 될때의 시간    board[i][j]이상 board[i][j]*2 미만 가능
 	}
 }
 
@@ -36,5 +89,30 @@ class Ceil{
 	private int x;
 	private int y;
 	private int time;
+	public Ceil(int x, int y, int time) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.time = time;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	
 }
 
