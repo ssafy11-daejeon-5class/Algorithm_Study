a0073a48de5e8cc1db77d0432cd62c532f3ebf4d
minkyung7365@gmail.com <minkyung7365@gmail.com>
2024-02-07 17:41:23 +0900

    boj_17136

--- src/hw/_240207/ming/boj_17136.java
+++ src/hw/_240207/ming/boj_17136.java
@@ -9,8 +9,10 @@ public class boj_17136 {
 	// 색종이 붙이기
 	static int[][] map = new int[10][10];
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		init();
 		
+		print();
 	}
 
 	public static void init() throws IOException {
@@ -20,6 +22,51 @@ public class boj_17136 {
 			for(int j=0 ; j<10 ; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
+		}
+	}
+
+	public static void findStart() {
+		L: for(int i=0 ; i<10 ; i++) {
+			for(int j=0 ; j<10 ; j++) {
+				if(map[i][j] == 1) {
+					break L;
+				}
+			}
+		}
+	}
+	
+	public static void recursive() {
+		// basis part
+		
+		
+		// inductive part
+		
+		
+		
+	}
+	
+	public static boolean checkOverwrite(int len, int x, int y, int[][] arr) {
+		
+		for(int i = x; i < x + len ; i++) {
+			for(int j = y ; j < y + len ; j++) {
+				if(arr[x][y] == 0) return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public static void print() {
+		
+	}
+	
+	public static class Node {
+		int x;
+		int y;
+		
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
 		}
 	}
 }
