2224e8ea7c203879ce35a157ba781396d8cc7071
minkyung7365@gmail.com <minkyung7365@gmail.com>
2024-02-13 13:35:37 +0900

    [ADD]HW 240213

---
+++ src/hw/_240213/ming/boj_1931.java
@@ -0,0 +1,67 @@
+package hw._240213.ming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class boj_1931 {
+    // 회의실 배정
+    private static int n;
+    private static Meeting[] meetings;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        System.out.println(count());
+    }
+    
+    public static void init() throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        meetings = new Meeting[n];
+
+        for(int i=0 ; i<n ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            meetings[i] = new Meeting(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+        Arrays.sort(meetings);
+    }
+    
+    public static int count() {
+    	int cnt = 1;
+    	int end = meetings[0].end;
+    	
+    	for(int i=1 ; i<n ; i++) {
+    		if(end <= meetings[i].start) {
+    			end = meetings[i].end;
+    			cnt++;
+    		}
+    	}
+    	
+    	return cnt;
+    }
+    
+    public static class Meeting implements Comparable<Meeting>{
+    	int start, end;
+
+		public Meeting(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		@Override
+		public int compareTo(Meeting o) {
+			return this.end != o.end ? this.end - o.end : this.start - o.start;
+		}
+
+		@Override
+		public String toString() {
+			return "Meeting [start=" + start + ", end=" + end + "]";
+		}
+		
+    }
+}
---
+++ src/hw/_240213/ming/boj_3040.java
@@ -0,0 +1,59 @@
+package hw._240213.ming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class boj_3040 {
+	// 백설 공주와 일곱 난쟁이
+	static int[] arr;
+	static int sum;
+	
+	public static void main(String[] args) throws IOException {
+		init();
+		find();
+		print();
+	}
+
+	public static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		arr = new int[9];
+		sum = 0;
+		
+		for(int i=0 ; i<9 ; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+	}
+
+	public static void find() {
+		L: for(int i=0 ; i<9 ; i++) {
+			for(int j=0 ; j<9 ; j++) {
+				if(i == j) continue;
+				
+				int temp = arr[i] + arr[j];
+				
+				if(sum - temp == 100) {
+					arr[i] = 0;
+					arr[j] = 0;
+					break L;
+				}
+			}
+		}
+	}
+
+	public static void print() {
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0 ; i<9 ; i++) {
+			if(arr[i] != 0)
+				sb.append(arr[i]).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
