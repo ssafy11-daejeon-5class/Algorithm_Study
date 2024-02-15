3d4f171da63708f89c5e602ea389219050133e54
kwonja <kownja87@gmail.com>
2024-02-15 19:55:51 +0900

    merge : 통합

--- src/hw/_240214/kwonja/BOJ_3109.java
+++ src/hw/_240214/kwonja/BOJ_3109.java
@@ -13,26 +13,69 @@ public class BOJ_3109 {
 	 */
 	static int[][] board;
 	static int r,c;
+	static int[] dx= {-1,0,1,0};
+	static int[] dy= {1,1,1,1};
+	static int arrive;
+	static int res;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		r=Integer.parseInt(st.nextToken());
 		c=Integer.parseInt(st.nextToken());
-		
+		board = new int[r][c];
 		for(int i=0;i<r;i++)
 		{
 			st= new StringTokenizer(br.readLine());
 			String str=st.nextToken();
 			for(int j=0;j<c;j++)
 			{
-				if(str.charAt(j)=='X')
+				if(str.charAt(j)=='x')
 				{
-					board[i][j]=1; //건물위치
+					board[i][j]=1; //건물위치를 1로 지정
 				}
 			}
 		}
-		
 		//파이프의 시작
+		//최대 개수를 구해야한다->그리디하게 생각?
+		//대각선위로 갈수 있다면 가고 그다음 대각선, 그다음 아래로
+		for(int i=0;i<r;i++)
+		{
+			arrive=0;
+			dfs(i,0);
+//			print();
+//			System.out.println();
+		}
+		System.out.println(res);
+	}
+	public static void dfs(int curx,int cury) {
+		if(cury==c-1) //파이프 끝에 도달
+		{
+			arrive=1;
+			res++;
+			return;
+		}
+		
+		for(int i=0;i<4;i++)
+		{
+			int nx= curx+dx[i];
+			int ny = cury+dy[i];
+			if(nx<0 || nx>=r || ny<0 || ny>=c)continue;
+			if(board[nx][ny]>0)continue; //건물이거나 방문했다면 패스
+			board[nx][ny]=2;
+			dfs(nx,ny);
+			if(arrive==1)break;
+		}
+	}
+	public static void print()
+	{
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+			{
+				System.out.print(board[i][j]);
+			}
+			System.out.println();
+		}
 	}
 
 }
