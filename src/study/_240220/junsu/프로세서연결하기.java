db7f51e3235d3fcee6cd962a74edc957c4622836
dlwnstn0524 <ljs052450@gmail.com>
2024-02-19 17:56:14 +0900

    [FEAT] 프로세서 연결하기 풀이중

--- src/study/_240220/junsu/프로세서연결하기.java
+++ src/study/_240220/junsu/프로세서연결하기.java
@@ -18,6 +18,7 @@ public class 프로세서연결하기 {
 			this.i = i;
 			this.j = j;
 		}
+
 		@Override
 		public String toString() {
 			// TODO Auto-generated method stub
@@ -25,12 +26,13 @@ public class 프로세서연결하기 {
 		}
 	}
 
-	static int T, N, answer, lineSum;
+	static int T, N, answerCnt, answer, lineSum, coreCnt;
 	static List<Node> cores;
-	static Node[] sel;
 	static StringTokenizer st;
 	static boolean[][] v;
 	static int[][] maps;
+	static int[] di = { -1, 1, 0, 0 };
+	static int[] dj = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws IOException {
 
@@ -38,6 +40,7 @@ public class 프로세서연결하기 {
 		T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++) {
 			answer = Integer.MAX_VALUE;
+			answerCnt = Integer.MIN_VALUE;
 
 			N = Integer.parseInt(br.readLine());
 			maps = new int[N][N];
@@ -46,16 +49,15 @@ public class 프로세서연결하기 {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					maps[i][j] = Integer.parseInt(st.nextToken());
-					if (maps[i][j] == 1 && i != 0 && i != N-1 && j != 0 && j != N-1) {
+					if (maps[i][j] == 1 && i != 0 && i != N - 1 && j != 0 && j != N - 1) {
 						cores.add(new Node(i, j));
 					}
 				}
 			}
-			
-			sel = new Node[cores.size()];
-			combi(0, cores.size());
-			
-			
+
+			coreCnt = cores.size();
+			combi(0, 0, 0, 0);
+
 //			for (int i = 0; i < cores.size(); i++) {
 //				sel = new Node[cores.size() - i];
 //				combi(0, cores.size() - i);
@@ -68,32 +70,21 @@ public class 프로세서연결하기 {
 
 	}
 
-	private static void combi(int depth, int cnt) {
-		if(cnt == 0) {
-			// 조합 발견 -> 비즈니스 로직 시작
-			lineSum = 0;
-			v = new boolean[N][N];
-			if(check(0, 0)) {
-				answer = Math.min(answer, lineSum);
+	private static void combi(int depth, int idx, int cnt, int lineCnt) {
+		// TODO Auto-generated method stub
+		if (depth == coreCnt) {
+			if (answerCnt < cnt) {
+				answerCnt = cnt;
+				answer = lineCnt;
+			} else if (answerCnt == cnt) {
+				answer = Math.min(answer, lineCnt);
 			}
 			return;
-		} if (depth == cores.size()) {
-			return;
 		}
-		
-		sel[sel.length - cnt] = cores.get(depth);
-		combi(depth + 1, cnt - 1);
-		combi(depth + 1, cnt);
-	}
 
-	private static boolean check(int depth, int sum) {
-		if(depth == sel.length) {
-			lineSum = sum;
-			return true;
-		}
+		// 체크해서 해당 방향으로 직진이 가능하다면 check() -> 재귀타고 돌아와서 unCheck() 해주고 제외하고 재귀한번 더 타기.
 		for (int i = 0; i < 4; i++) {
-			
+
 		}
-		return false;
 	}
 }
