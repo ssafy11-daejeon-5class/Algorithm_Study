338d48a689252af7b7fddf310a272ba2c5d2c1a3
Jeonghyeon Kim <6843wjdgus@naver.com>
2024-02-20 09:37:43 +0900

    feat: 240221 study 말숭이 컷

--- src/study/_240220/jeonghyeon/말이되고픈권성민.java
+++ src/study/_240220/jeonghyeon/말이되고픈권성민.java
@@ -13,7 +13,7 @@ public class 말이되고픈권성민 {
 	static int R;
 	static int C;
 	static char[][] map;
-	static int[][] visited;
+	static boolean[][][] visited;
 	static int[] dr = new int[] { 1, 2, -1, 2, 0, 1, -1, 0, -2, 1, -2, -1 }; // 0~3, 8~11은 말, 4~7은 원숭이
 	static int[] dc = new int[] { 2, 1, 2, -1, 1, 0, 0, -1, 1, -2, -1, -2 };
 
@@ -24,7 +24,7 @@ public class 말이되고픈권성민 {
 		C = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
 		map = new char[R][C];
-		visited = new int[R][C];
+		visited = new boolean[R][C][K + 1];
 
 		for (int i = 0; i < R; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -33,15 +33,19 @@ public class 말이되고픈권성민 {
 			}
 		}
 
-		bfs();
+		if (R == 1 && C == 1) {
+			System.out.println(0);
+			return;
+		}
 
+		bfs();
 	}
 
 	private static void bfs() {
 
 		Queue<Point> queue = new ArrayDeque<Point>();
 		queue.offer(new Point(0, 0));
-		visited[0][0] = 0;
+		visited[0][0][0] = true;
 
 		while (!queue.isEmpty()) {
 
@@ -50,17 +54,12 @@ public class 말이되고픈권성민 {
 			for (int i = 0; i < dr.length; i++) {
 				int nr = p.r + dr[i];
 				int nc = p.c + dc[i];
-				int ncnt = p.cnt + 1;
-				
-				//ㅇㅇ
-				if(ncnt == 5) return;
-				//ㅐㅐ
-				
+				int ncnt = p.jumpCnt + 1;
 				int nhj;
 				if ((i < 4 || i >= 8))
-					nhj = p.jumps + 1;
+					nhj = p.horseJumpCnt + 1;
 				else
-					nhj = p.jumps;
+					nhj = p.horseJumpCnt;
 
 				if (notInArea(nr, nc)) // 맵 벗어났을 때
 					continue;
@@ -68,29 +67,18 @@ public class 말이되고픈권성민 {
 					continue;
 				if (nhj > K)
 					continue;
+				if (visited[nr][nc][nhj])
+					continue;
 
 				if (nr == R - 1 && nc == C - 1) {
-					System.out.println("cnt:" + ncnt + ", nr:" + nr + ", nc:" + nc + ", horse:" + nhj);
-					System.out.println(ncnt + " answer");
+					System.out.println(ncnt);
 					return;
 				}
 
-				System.out.println("cnt:" + p.cnt + ", nr:" + nr + ", nc:" + nc + ", horse:" + nhj);
-
 				Point np;
-				if (i >= 4 && i < 8 && !someoneVisitedLessOrSameHorseJumpThanYou(nr, nc, nhj)) {
-					// 원숭이일 경우
-					visited[nr][nc] = visited[p.r][p.c];
-					np = new Point(nr, nc, ncnt, nhj);
-				} else if (p.jumps < K && !someoneVisitedLessOrSameHorseJumpThanYou(nr, nc, nhj)) {
-					// 말점프 K번 미만으로 했고 기존에 더 적거나 같은 말점프 횟수로 방문한 적이 없을 때
-					visited[nr][nc] = visited[p.r][p.c] + (int) Math.pow(2, nhj);
-					np = new Point(nr, nc, ncnt, nhj);
-				} else {
-					visited[nr][nc] = visited[p.r][p.c];
-					continue;
-				}
+				np = new Point(nr, nc, ncnt, nhj);
 				queue.offer(np);
+				visited[nr][nc][nhj] = true;
 
 			}
 
@@ -106,28 +94,22 @@ public class 말이되고픈권성민 {
 			return true;
 		return false;
 	}
-
-	public static boolean someoneVisitedLessOrSameHorseJumpThanYou(int r, int c, int horseJumps) {
-		if ((long) visited[r][c] % ((long) Math.pow(2, horseJumps + 1)) > 0)
-			return true;
-		return false;
-	}
 }
 
 class Point {
-	int r, c, cnt, jumps;
+	int r, c, jumpCnt, horseJumpCnt;
 
 	Point(int r, int c) {
 		this.r = r;
 		this.c = c;
-		this.cnt = 0;
-		this.jumps = 0;
+		this.jumpCnt = 0;
+		this.horseJumpCnt = 0;
 	}
 
 	Point(int r, int c, int cnt, int jumps) {
 		this.r = r;
 		this.c = c;
-		this.cnt = cnt;
-		this.jumps = jumps;
+		this.jumpCnt = cnt;
+		this.horseJumpCnt = jumps;
 	}
 }
