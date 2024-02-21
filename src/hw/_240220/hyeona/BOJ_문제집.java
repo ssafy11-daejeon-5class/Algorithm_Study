47f21672ba4fc838d0d9365c084df67d5ae850e7
chahyeonnaa <gusdk2150@naver.com>
2024-02-21 11:03:32 +0900

    feat: 문제집

--- src/hw/_240220/hyeona/BOJ_문제집.java
+++ src/hw/_240220/hyeona/BOJ_문제집.java
@@ -10,7 +10,19 @@
 package hw._240220.hyeona;
 
 
+/*
+6 7
+5 6
+5 2
+2 1
+6 1
+2 4
+1 3
+4 3
 
+ans : 5 2 4 6 1 3
+
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -19,6 +31,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -29,7 +42,7 @@ public class BOJ_문제집 {
 	static int[] inDegree;
 	static boolean[] visited;
 	static List<Integer>[] list;
-	static Queue<Integer> queue = new LinkedList<>();
+	static PriorityQueue<Integer> queue = new PriorityQueue<>();
 	
 	static StringBuilder sb = new StringBuilder();
 	
@@ -46,7 +59,6 @@ public class BOJ_문제집 {
 		
 		list = new ArrayList[N+1];
 		inDegree = new int[N+1];
-		visited = new boolean[N+1];
 		
 		for(int i=0; i<N+1; i++)
 		{
@@ -65,19 +77,12 @@ public class BOJ_문제집 {
 			
 		}
 		
-		for(int i=1; i<=N; i++)
-		{
-			Collections.sort(list[i]);
-		}
-	
 
 		for(int i=1; i<=N; i++)
 		{
 			if(inDegree[i]==0)
 			{
 				queue.offer(i);
-				visited[i] = true;
-				break;
 			}
 		}
 		
@@ -90,37 +95,19 @@ public class BOJ_문제집 {
 				
 		while(!queue.isEmpty())
 		{
-			
 			int current = queue.poll();
 			sb.append(current).append(" ");
 			
-			for(int k=1; k<=N; k++)
-			{
-				if(inDegree[k]==0 && !visited[k])
-				{
-					queue.offer(k);
-					visited[k] = true;
-					break;
-				}
-			}
-			
-			// sb.append(current);
-			
+			// current 정점과 연결된 정점 차수 줄이기
 			for(int i=0; i<list[current].size(); i++)
 			{
 				inDegree[list[current].get(i)]--;
-				
-				
+
 				if(inDegree[list[current].get(i)]==0)
 				{
-					visited[list[current].get(i)] = true;
 					queue.offer(list[current].get(i));
-					
 				}
 			}
-			
-			
-			
 		}
 
 	}
