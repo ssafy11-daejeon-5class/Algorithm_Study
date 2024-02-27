ae994d5b00e70cb288f98f0f0dc0fa76c439a1f6
dlwnstn0524 <ljs052450@gmail.com>
2024-02-27 09:36:36 +0900

    [FEAT] 미세먼지 공기청정 구현 완료

--- src/study/_240227/junsu/미세먼지안녕.java
+++ src/study/_240227/junsu/미세먼지안녕.java
@@ -48,7 +48,7 @@ public class 미세먼지안녕 {
 		}
 		
 		for (int i = 0; i < T; i++) {
-			maps = spread();
+//			maps = spread();
 			maps = refresh();
 			System.out.println();
 			print();
@@ -66,9 +66,8 @@ public class 미세먼지안녕 {
 						int ni = i + di[k];
 						int nj = j + dj[k];
 						if(ni >= 0 && ni < R && nj >= 0 && nj < C){
-							if(maps[ni][nj] != -1){
-								count++;
-								
+							if(maps[ni][nj] != -1 && maps[ni][nj] != 0){
+								continue;
 							}
 						}
 					}
@@ -85,7 +84,36 @@ public class 미세먼지안녕 {
 	private static int[][] refresh() { 
 		int[][] res = new int[R][C];
 		Node up = refresher[0];		// 반시계방향으로 도는 위쪽 공기청정기
+		for (int i = 1; i < C; i++) {
+			res[up.i][i] = maps[up.i][i-1];
+		}
+		for (int i = up.i; i > 0 ; i--) {
+			res[i-1][C-1] = maps[i][C-1];
+		}
+		for (int i = C-1; i > 0 ; i--) {
+			res[0][i-1] = maps[0][i];
+		}
+		for (int i = 0; i < up.i ; i++) {
+			res[i+1][0] = maps[i][0];
+		}
+		res[up.i][up.j] = -1;
+		res[up.i][up.j+1] = 0;
+		
 		Node down = refresher[1];	// 시계방향으로 도는 아래쪽 공기청정기
+		for (int i = 1; i < C; i++) {
+			res[down.i][i] = maps[down.i][i-1];
+		}
+		for (int i = down.i; i < R-1 ; i++) {
+			res[i+1][C-1] = maps[i][C-1];
+		}
+		for (int i = C-1; i > 0 ; i--) {
+			res[R-1][i-1] = maps[R-1][i];
+		}
+		for (int i = R-1; i > down.i ; i--) {
+			res[i-1][0] = maps[i][0];
+		}
+		res[down.i][down.j] = -1;
+		res[down.i][down.j+1] = 0;
 		return res;
 	}
 
