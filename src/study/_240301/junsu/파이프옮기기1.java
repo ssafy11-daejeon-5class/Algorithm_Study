aa4e3401c5aebf3fb37c3673609427ab9329e09c
dlwnstn0524 <ljs052450@gmail.com>
2024-03-01 10:13:55 +0900

    [REFACTOR] 파이프를 옮기자

--- src/study/_240301/junsu/파이프옮기기1.java
+++ src/study/_240301/junsu/파이프옮기기1.java
@@ -1,43 +1,68 @@
-package study._240228.junsu;
+package ws._240229;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class 파이프옮기기1 {
 
 	static int N, answer;
-	static char[][] maps;
+	static int[][] maps;
 	static StringTokenizer st;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 
-		maps = new char[N][N];
+		maps = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
-				maps[i][j] = st.nextToken().charAt(0);
+				maps[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		recursive(0, 0, 0);
-
+		answer = 0;
+		recursive(0, 1, 0);
 		System.out.println(answer);
 	}
 
-	static int[] dr = {0, 1, 1};
-	static int[] dc = {1, 1, 0};
-	
-	private static void recursive(int r, int c, int direction) {
+	private static void recursive(int r, int c, int dir) {
 		if (r == N - 1 && c == N - 1) {
+			answer++;
 			return;
 		}
-		if(r+1 )
-		
+		switch(dir) {
+		case 0:	
+			// 가로 방향으로 들어왔을때 -> 가로, 대각선
+			if(c+1 < N && maps[r][c+1] == 0) {
+				recursive(r, c+1, 0);
+			}
+			if(r+1 < N && c+1 < N && maps[r][c+1] == 0 && maps[r+1][c] == 0 && maps[r+1][c+1] == 0) {
+				recursive(r+1, c+1, 1);
+			}
+			break;
+		case 1:
+			// 대각선 방향으로 들어왔을때 -> 가로, 대각선, 세로
+			if(c+1 < N && maps[r][c+1] == 0) {
+				recursive(r, c+1, 0);
+			}
+			if(r+1 < N && c+1 < N && maps[r][c+1] == 0 && maps[r+1][c] == 0 && maps[r+1][c+1] == 0) {
+				recursive(r+1, c+1, 1);
+			}
+			if(r+1 < N && maps[r+1][c] == 0) {
+				recursive(r+1, c, 2);
+			}
+			break;
+		case 2:
+			// 세로 방향으로 들어왔을때 -> 대각선, 세로
+			if(r+1 < N && c+1 < N && maps[r][c+1] == 0 && maps[r+1][c] == 0 && maps[r+1][c+1] == 0) {
+				recursive(r+1, c+1, 1);
+			}
+			if(r+1 < N && maps[r+1][c] == 0) {
+				recursive(r+1, c, 2);
+			}
+			break;
+		}
 	}
-
 }
