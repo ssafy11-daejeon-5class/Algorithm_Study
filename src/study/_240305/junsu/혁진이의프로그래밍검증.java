f69859afc75b009568076a35836a16d2b6c78429
dlwnstn0524 <ljs052450@gmail.com>
2024-03-08 17:23:14 +0900

    [ADD] 0308

--- src/study/_240305/junsu/혁진이의프로그래밍검증.java
+++ src/study/_240305/junsu/혁진이의프로그래밍검증.java
@@ -32,7 +32,7 @@ public class 혁진이의프로그래밍검증 {
 
 			print();
 			answer = "NO";
-			func();
+//			recur();
 			System.out.printf("#%d %s\n", t, answer);
 		}
 	}
@@ -41,63 +41,64 @@ public class 혁진이의프로그래밍검증 {
 	static int[] dj = {1, 0, -1, 0};
 	static 
 
-	private static void func() {
-		Queue<int[]> q = new ArrayDeque<>();
-		boolean[][][][] v = new boolean[R][C][4][16];
-		q.offer(new int[]{0, 0, 0, 0});
-
-		while(!q.isEmpty()){
-			int[] cur = q.poll();
-
-			if(v[cur[0]][cur[1]][cur[2]][cur[3]]){
-				continue;
-			}
-			v[cur[0]][cur[1]][cur[2]][cur[3]] = true;
-			int[] next = new int[4];
-			next[2] = cur[2]; 
-
-			if(maps[cur[0]][cur[1]] == '<'){
-				next[3] = 2;				
-			} else if(maps[cur[0]][cur[1]] == '>'){
-				next[3] = 0;
-			} else if(maps[cur[0]][cur[1]] == '^'){
-				next[3] = 3;
-			} else if(maps[cur[0]][cur[1]] == 'v'){
-				next[3] = 1;
-			} else if(maps[cur[0]][cur[1]] == '_'){
-				next[3] = cur[2]==0?0:2;
-			} else if(maps[cur[0]][cur[1]] == '|'){
-				next[3] = cur[2]==0?1:3;
-			} else if(maps[cur[0]][cur[1]] == '?'){
-				
-			} else if(maps[cur[0]][cur[1]] == '@'){
-				answer = "YES";
-				break;
-			} else if(maps[cur[0]][cur[1]] == '+'){
-				next[2] = cur[2]==15?0:cur[2]+1;
-			} else if(maps[cur[0]][cur[1]] == '-'){
-				next[2] = cur[2]==0?15:cur[2]-1;
-			} else {
-				next[2] = Integer.parseInt(maps[cur[0]][cur[1]] +"");
-			}
-
-			next[0] = cur[0] + di[next[3]];
-			next[1] = cur[1] + dj[next[3]];
-			if(next[0] == -1){
-				next[0] = R - 1;
-			}
-			if(next[0] == R){
-				next[0] = 0;
-			}
-			if(next[1] == -1){
-				next[1] = C - 1;
-			}
-			if(next[1] == C){
-				next[1] = 0;
-			}
-			q.offer(next);
-		}		
-	}
+//	private static void recur() {
+//		Queue<int[]> q = new ArrayDeque<>();
+//		boolean[][][][] v = new boolean[R][C][4][16];
+//		q.offer(new int[]{0, 0, 0, 0});
+//
+//		while(!q.isEmpty()){
+//			int[] cur = q.poll();
+//
+//			if(v[cur[0]][cur[1]][cur[2]][cur[3]]){
+//				continue;
+//			}
+//			v[cur[0]][cur[1]][cur[2]][cur[3]] = true;
+//			int[] next = new int[4];
+//			next[2] = cur[2]; 
+//
+//			if(maps[cur[0]][cur[1]] == '<'){
+//				next[3] = 2;				
+//			} else if(maps[cur[0]][cur[1]] == '>'){
+//				next[3] = 0;
+//			} else if(maps[cur[0]][cur[1]] == '^'){
+//				next[3] = 3;
+//			} else if(maps[cur[0]][cur[1]] == 'v'){
+//				next[3] = 1;
+//			} else if(maps[cur[0]][cur[1]] == '_'){
+//				next[3] = cur[2]==0?0:2;
+//			} else if(maps[cur[0]][cur[1]] == '|'){
+//				next[3] = cur[2]==0?1:3;
+//			} else if(maps[cur[0]][cur[1]] == '?'){
+//				
+//			} else if(maps[cur[0]][cur[1]] == '@'){
+//				answer = "YES";
+//				break;
+//			} else if(maps[cur[0]][cur[1]] == '+'){
+//				next[2] = cur[2]==15?0:cur[2]+1;
+//			} else if(maps[cur[0]][cur[1]] == '-'){
+//				next[2] = cur[2]==0?15:cur[2]-1;
+//			} else {
+//				next[2] = Integer.parseInt(maps[cur[0]][cur[1]] +"");
+//			}
+//
+//			next[0] = cur[0] + di[next[3]];
+//			next[1] = cur[1] + dj[next[3]];
+//			if(next[0] == -1){
+//				next[0] = R - 1;
+//			}
+//			if(next[0] == R){
+//				next[0] = 0;
+//			}
+//			if(next[1] == -1){
+//				next[1] = C - 1;
+//			}
+//			if(next[1] == C){
+//				next[1] = 0;
+//			}
+//			q.offer(next);
+//		}
+//		return;
+//	}
 
 	private static void print() {
 		for (int i = 0; i < R; i++) {
